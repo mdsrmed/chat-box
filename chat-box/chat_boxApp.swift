398b2611eab75755aa3e6ed65391bf77c2bc0d01
mdsrmed <mdsrmed@gmail.com>
2023-09-02 17:45:47 -0400

    Initial Commit

---
+++ chat-box/chat_boxApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_boxApp.swift
+//  chat-box
+//
+//  Created by Md Shohidur Rahman on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct chat_boxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
